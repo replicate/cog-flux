4e8d3929624599a39977893d9dcaafed250c1b53
fofr <fofr@users.noreply.github.com>
2024-08-27 06:09:50 +0100

    Fix disable safety checker input (#10)

--- predict.py
+++ predict.py
@@ -45,7 +45,7 @@ class SharedInputs:
             le=100,
         )
     disable_safety_checker: Input = Input(
-            description="Disable safety checker for generated images. This feature is only available through the API. See [https://replicate.com/docs/how-does-replicate-work#safety](https://replicate.com/docs/how-does-replicate-work#safety)",
+            description="Disable safety checker for generated images.",
             default=False,
     )
 
