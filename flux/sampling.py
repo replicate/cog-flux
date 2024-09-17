d24e7014942f5c195ebe75b4fc2f68c7d5b4023f
Dan Nelson <dan.nelson8@gmail.com>
2024-09-17 04:08:45 +0000

    don't need max autotune

--- flux/sampling.py
+++ flux/sampling.py
@@ -121,7 +121,7 @@ def denoise_single_item(
         torch._dynamo.mark_dynamic(img_ids, 1, min=256, max=8100)
         model = model.to(memory_format=torch.channels_last)
         quantize_(model, float8_dynamic_activation_float8_weight(granularity=PerRow()))
-        model = torch.compile(model, mode="max-autotune", fullgraph=True)
+        model = torch.compile(model)
 
     for t_curr, t_prev in tqdm(zip(timesteps[:-1], timesteps[1:])):
         t_vec = torch.full((1,), t_curr, dtype=img.dtype, device=img.device)
