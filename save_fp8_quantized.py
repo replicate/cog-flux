573351196204a192a1b5b04ff69cd8b45c793660
dan nelson <dan.nelson8@gmail.com>
2025-01-10 18:13:11 +0000

    saving script

---
+++ save_fp8_quantized.py
@@ -0,0 +1,78 @@
+# right. so what should this do? 
+# basically we need to load the model and quantize
+# then persist it. 
+
+import os
+from predict import DevPredictor
+from fp8.util import ModelVersion
+
+
+
+def generate_base_img(p, img_name='cool_dog_1234.png'):
+    out = p.predict('a cool dog', '1:1', None, 0, 1, 28, 3, 1234, 'png', 100, True, True, "1")
+    # for f in out:
+    #     f.save('cool_dog_1234.png')
+    os.system(f'mv out-0.png {img_name}')
+
+def save_dev_fp8():
+    p = DevPredictor()
+    p.base_setup("flux-dev", compile_fp8=True)
+
+    from safetensors.torch import save_file
+
+    fp8_weights_path = 'model-cache/dev-fp8'
+    if not os.path.exists(fp8_weights_path):
+        os.makedirs(fp8_weights_path)
+
+    # warmup_dict = dict(
+    #     prompt="A beautiful test image used to solidify the fp8 nn.Linear input scales prior to compilation 😉",
+    #     height=768,
+    #     width=768,
+    #     num_steps=12,
+    #     guidance=3.5,
+    #     seed=10,
+    # )
+    # if p.fp8_pipe.config.version == ModelVersion.flux_schnell:
+    #     print("schnell")
+    #     warmup_dict["num_steps"] = 4
+    #     for _ in range(3):
+    #         p.fp8_pipe.generate(**warmup_dict)
+    # else:
+    #     print("dev")
+    #     p.fp8_pipe.generate(**warmup_dict)
+
+    generate_base_img(p)
+    print("scale initialized: ", p.fp8_pipe.model.double_blocks[0].img_mod.lin.input_scale_initialized)
+    sd = p.fp8_pipe.model.state_dict()
+    to_trim = '_orig_mod.'
+    sd_to_save = {k[len(to_trim):] : v for k, v in sd.items()}
+    save_file(sd_to_save, fp8_weights_path + '/' + 'dev_fp8.safetensors')
+
+# ok. so if that's saved, then we need to load it. 
+# what does that look like? changing the config, I believe. 
+def test_dev_fp8():
+    p = DevPredictor()
+    p.base_setup("flux-dev-fp8", compile_fp8=True)
+    # import pdb
+    # pdb.set_trace()
+    # warmup_dict = dict(
+    #     prompt="A beautiful test image used to solidify the fp8 nn.Linear input scales prior to compilation 😉",
+    #     height=768,
+    #     width=768,
+    #     num_steps=12,
+    #     guidance=3.5,
+    #     seed=10,
+    # )
+    # print("dev")
+    # p.fp8_pipe.generate(**warmup_dict)
+    #p.compile_fp8()
+    generate_base_img(p, 'cool_dog_1234_loaded_from_compiled.png')
+
+def test_dev_base(i):
+    p = DevPredictor()
+    p.setup()
+    generate_base_img(p, f'cool_dog_1234_compiled_{i}.png')
+
+if __name__ == '__main__':
+    save_dev_fp8()
+    test_dev_fp8()
