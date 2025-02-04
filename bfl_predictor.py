25dc6d76df86a29a928af05026a783e2db092612
dan nelson <dan.nelson8@gmail.com>
2025-02-04 18:45:54 +0000

    proper scaling for go_fast loras

--- bfl_predictor.py
+++ bfl_predictor.py
@@ -75,6 +75,8 @@ class LoraMixin:
             lora_scale = extra_lora_scale
             extra_lora_weights = None
 
+        lora_scale = lora_scale * self.lora_scale_multiplier
+        print("lora scale", lora_scale)
         cur_lora = self.lora
         cur_extra_lora = self.extra_lora
 
